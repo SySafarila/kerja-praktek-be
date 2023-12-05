1863b51a94b5103f9d9437d7f1af8bdac35e531e
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2023-12-05 11:06:49 +0700

    update navbar toggle

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -4,4 +4,9 @@ const navbarMenus = document.getElementById("navbar-menus");
 navbarToggle.addEventListener("click", (e) => {
     e.preventDefault();
     navbarMenus.classList.toggle("hidden");
+    if (!navbarMenus.classList.contains("hidden")) {
+        navbarToggle.innerText = 'close';
+    } else {
+        navbarToggle.innerText = 'menu';
+    }
 });
--- resources/views/components/public/navbar.blade.php
+++ resources/views/components/public/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-accent-1 fixed w-full top-0 left-0 z-50">
     <div class="max-w-screen-lg mx-auto flex flex-col lg:flex-row justify-between px-5 py-2 lg:py-1.5">
         <div class="flex justify-between w-full">
-            <a href="/" class="flex gap-2 items-center">
+            <a href="/" class="flex gap-2 items-center hover:no-underline">
                 <img src="{{ asset('images/logo.png') }}" alt="" class="h-12 w-auto">
                 <div class="flex flex-col justify-center">
                     <span class="font-bold text-accent-2">SMA Ma'arif</span>
