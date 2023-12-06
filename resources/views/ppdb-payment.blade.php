48ea713b6e2abf7557edecd37238216d39ca3893
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2023-12-06 15:06:34 +0700

    update ppdb payment

--- resources/views/ppdb-payment.blade.php
+++ resources/views/ppdb-payment.blade.php
@@ -13,23 +13,33 @@
         <div class="flex flex-col gap-3">
             <div class="flex flex-col gap-3">
                 <span class="text-xl font-semibold">Ayo selesaikan pendaftaran kamu</span>
-                <div class="flex justify-between items-center border-b border-accent-3 pb-3">
+                <div class="flex justify-between items-center border-b border-accent-3 pb-3" id="payment_va">
                     <span class="font-semibold">BCA Virtual Account</span>
                     <img src="{{ asset('images/BCA.png') }}" alt="" class="h-[24px] w-auto">
                 </div>
-                <div class="flex justify-between items-center border-b border-accent-3 pb-3 hidden">
+                <div class="flex justify-between items-center border-b border-accent-3 pb-3 hidden" id="payment_school">
+                    <span class="font-semibold">Bayar Di Sekolah</span>
+                    <img src="{{ asset('images/logo.png') }}" alt="" class="h-[24px] w-auto">
+                </div>
+                <div class="flex justify-between items-center border-b border-accent-3 pb-3 hidden" id="payment_qris">
                     <span class="font-semibold">QRIS</span>
                     <img src="{{ asset('images/qris.png') }}" alt="" class="h-[24px] w-auto">
                 </div>
             </div>
-            <div class="flex flex-col">
+            <div class="flex flex-col" id="payment_va_detail">
                 <span>Nomor Virtual Account</span>
                 <div class="flex justify-between items-center">
-                    <span class="text-xl font-semibold">80777082117694132</span>
-                    <span class="uppercase cursor-pointer text-accent-1 font-semibold">Copy</span>
+                    <span class="text-xl font-semibold" id="va">80777082117694132</span>
+                    <span class="uppercase cursor-pointer text-accent-1 font-semibold" id="copy_va">Copy</span>
                 </div>
             </div>
-            <div class="flex flex-col hidden">
+            <div class="flex flex-col hidden" id="payment_school_detail">
+                <span>Nomor Pendaftaran</span>
+                <div class="flex justify-between items-center">
+                    <span class="text-xl font-semibold">123123</span>
+                </div>
+            </div>
+            <div class="flex flex-col hidden" id="payment_qris_detail">
                 <span class="block text-center">Scan QR Code</span>
                 <img src="{{ asset('images/qr.png') }}" alt="" class="w-40 mx-auto">
                 <p class="text-center">Dapat digunakan dengan GoPay, OVO, dan aplikasi sejenis yang mendukung QRIS</p>
@@ -38,7 +48,7 @@
                 <div>
                     <span>Total Pembayaran</span>
                     <div class="flex justify-between items-center">
-                        <span class="text-xl font-semibold">Rp 20,000</span>
+                        <span class="text-xl font-semibold">Rp 150,000</span>
                     </div>
                 </div>
                 <div>
@@ -50,11 +60,16 @@
             </div>
             <div class="flex justify-between gap-5 lg:flex-col lg:gap-3">
                 <a href="#" class="btn bg-accent-1 text-accent-4 w-full text-center capitalize">Hubungi admin</a>
-                <a href="#" class="btn bg-white border border-accent-1 text-accent-1 w-full text-center capitalize">Refresh</a>
+                <a href="#"
+                    class="btn bg-white border border-accent-1 text-accent-1 w-full text-center capitalize">Refresh</a>
             </div>
         </div>
         <div class="flex flex-col gap-3">
             <span class="text-xl font-semibold">Data Calon Siswa Terkait</span>
+            <div class="flex flex-col">
+                <span class="block">NISN</span>
+                <span class="text-xl font-semibold block">000 000 000</span>
+            </div>
             <div class="flex flex-col">
                 <span class="block">Nama Siswa</span>
                 <span class="text-xl font-semibold block">Syahrul Safarila</span>
@@ -67,16 +82,26 @@
         <form action="#" method="POST" class="flex flex-col gap-3">
             <span class="text-xl font-semibold">Upload Berkas</span>
             <div class="flex flex-col gap-1.5">
-                <label id="akta" class="font-semibold capitalize">Akta Kelahiran <span class="text-red-500">*</span></label>
-                <input type="file" name="" id="akta" class="file:bg-white file:border file:border-accent-1 file:rounded-md file:px-5 file:py-1.5">
+                <label id="kk" class="font-semibold capitalize">Kartu Keluarga <span
+                        class="text-red-500">*</span></label>
+                <input type="file" name="" id="kk"
+                    class="file:bg-white file:border file:border-accent-1 file:rounded-md file:px-5 file:py-1.5">
+            </div>
+            <div class="flex flex-col gap-1.5">
+                <label id="akta" class="font-semibold capitalize">Akta Kelahiran <span
+                        class="text-red-500">*</span></label>
+                <input type="file" name="" id="akta"
+                    class="file:bg-white file:border file:border-accent-1 file:rounded-md file:px-5 file:py-1.5">
             </div>
             <div class="flex flex-col gap-1.5">
                 <label id="kip" class="font-semibold capitalize">KIP</label>
-                <input type="file" name="" id="kip" class="file:bg-white file:border file:border-accent-1 file:rounded-md file:px-5 file:py-1.5">
+                <input type="file" name="" id="kip"
+                    class="file:bg-white file:border file:border-accent-1 file:rounded-md file:px-5 file:py-1.5">
             </div>
             <div class="flex flex-col gap-1.5">
                 <label id="pkh" class="font-semibold capitalize">PKH</label>
-                <input type="file" name="" id="pkh" class="file:bg-white file:border file:border-accent-1 file:rounded-md file:px-5 file:py-1.5">
+                <input type="file" name="" id="pkh"
+                    class="file:bg-white file:border file:border-accent-1 file:rounded-md file:px-5 file:py-1.5">
             </div>
             <button type="submit" class="btn bg-accent-1 text-accent-4 lg:w-fit">Upload</button>
         </form>
@@ -85,4 +110,15 @@
 @endsection
 
 @section('script')
+<script>
+    const copy_va = document.getElementById('copy_va')
+    const va = document.getElementById('va')
+
+    copy_va.addEventListener('click', (e) => {
+        e.preventDefault()
+        navigator.clipboard.writeText(va.innerText)
+
+        alert('Virtual Account berhasil di copy!')
+    })
+</script>
 @endsection
