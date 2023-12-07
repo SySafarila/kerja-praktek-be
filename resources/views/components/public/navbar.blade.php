e8b9a9975f718150181f3b9c8b627795d65d0448
Muhammad Dagistan Silawane (Razes) <71701718+iamrazes@users.noreply.github.com>
2023-12-07 18:49:39 +0700

    login and register view

--- resources/views/components/public/navbar.blade.php
+++ resources/views/components/public/navbar.blade.php
@@ -2,20 +2,22 @@
     <div class="max-w-screen-lg mx-auto flex flex-col lg:flex-row justify-between px-5 py-2 lg:py-1.5">
         <div class="flex justify-between w-full">
             <a href="/" class="flex gap-2 items-center hover:no-underline">
-                <img src="{{ asset('images/logo.png') }}" alt="" class="h-12 w-auto">
+                <img src="{{ asset('images/logo-white-text.png') }}" alt="" class="h-12 w-auto">
                 <div class="flex flex-col justify-center">
-                    <span class="font-bold text-accent-2">SMA Ma'arif</span>
-                    <span class="font-light -mt-1.5 text-accent-2">Pacet Cianjur</span>
+                    <span class="font-light text-sm text-accent-2">Lembaga Pendidikan NU</span>
+                    <span class="font-bold text-accent-2 -mt-1">SMA MA'ARIF PACET</span>
                 </div>
             </a>
             <button type="button" class="material-icons lg:hidden text-accent-4" id="navbar-menu">menu</button>
         </div>
         <div class="flex flex-col lg:flex lg:flex-row lg:items-center lg:gap-8 gap-2 py-2.5 lg:py-0 hidden"
-            id="navbar-menus">
-            <a href="/" class="text-accent-2">Home</a>
-            <a href="#" class="text-accent-2">About</a>
-            <a href="#" class="text-accent-2">News</a>
-            <a href="#" class="text-accent-2">Contact</a>
+            id="navbar-menus" style="text-decoration: none">
+            <a href="/" class="text-accent-2" style="text-decoration: none">Home</a>
+            <a href="#" class="text-accent-2" style="text-decoration: none">News</a>
+            <a href="#" class="text-accent-2" style="text-decoration: none">About</a>
+            <a href="#" class="text-accent-2" style="text-decoration: none">Contact</a>
+            <a href="#" class="text-accent-2" style="text-decoration: none">eLibrary</a>
+
         </div>
     </div>
 </nav>
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -1,11 +1,14 @@
 <!doctype html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <link rel="shortcut icon" href="{{asset('images/logo.png')}}" type="image/x-icon">
+    <title>SMA Ma'arif Pacet Cianjur</title>
     @yield('head')
 
     @vite('resources/css/app.css')
