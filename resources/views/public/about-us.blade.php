c3ad42626846b274983ee131c0dcc9f27ddb8cb1
Muhammad Dagistan Silawane (Razes) <71701718+iamrazes@users.noreply.github.com>
2023-12-12 17:57:00 +0700

    about us done

--- resources/views/public/about-us.blade.php
+++ resources/views/public/about-us.blade.php
@@ -4,6 +4,120 @@
 @endsection
 
 @section('content')
+    <div class="flex flex-col gap-y-8 my-12 ">
+        <div class="flex flex-col gap-y-4 max-w-screen-lg mx-auto mt-8">
+            <img src="{{ asset('images/logos/logo1a.png') }}" class="h-60 w-60 mx-auto" alt="">
+            <h1 class="text-3xl font-bold text-center">SMA MA'ARIF PACET<br> CIANJUR</h1>
+        </div>
+        <div class="flex max-w-screen-lg mx-auto">
+            <div class="flex flex-col gap-y-4 px-4">
+                <div>
+                    <p class="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate</p>
+                </div>
+                <div>
+                    <h2 class="font-bold text-accent-1">VISI</h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odio
+                    </p>
+                </div>
+                <div>
+                    <h2 class="font-bold text-accent-1">MISI</h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odio
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex max-w-screen-lg mx-auto">
+            <div class="max-w-screen-sm px-4">
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla, cupiditate
+                    odio
+                    illum. Quam voluptatibus unde, praesentium quisquam cupiditate doloremque. Impedit reiciendis suscipit
+                    facere. Distinctio, aspernatur suscipit.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+                    quisquam ut possimus nulla, cupiditate odio
+                    illum. Quam voluptatibus unde, praesentium quisquam cupiditate doloremque. Impedit reiciendis suscipit
+                    facere. Distinctio, aspernatur suscipit.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+                    quisquam ut possimus nulla, cupiditate odio
+                    illum. Quam voluptatibus unde, praesentium quisquam cupiditate doloremque. Impedit reiciendis suscipit
+                    facere. Distinctio, aspernatur suscipit.
+                    <br>
+                    <br>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla, cupiditate
+                    odio
+                    illum. Quam voluptatibus unde, praesentium quisquam cupiditate doloremque. Impedit reiciendis suscipit
+                    facere. Distinctio, aspernatur suscipit.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+                    quisquam ut possimus nulla, cupiditate odio
+                    illum. Quam voluptatibus unde, praesentium quisquam cupiditate doloremque. Impedit reiciendis suscipit
+                    facere. Distinctio, aspernatur suscipit.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+                    quisquam ut possimus nulla, cupiditate odio
+                    illum. Quam voluptatibus unde, praesentium quisquam cupiditate doloremque. Impedit reiciendis suscipit
+                    facere. Distinctio, aspernatur suscipit.
+                </p>
+            </div>
+            <div class="flex flex-col px-4">
+                <img src="{{ asset('images/profile.png') }}" class="h-90 w-90 object-cover mx-auto border rounded-lg mb-4">
+                <p class="text-center">"
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla, cupiditate
+                    odio
+                    illum. Quam voluptatibus unde, praesentium quisquam cupiditate doloremque. Impedit reiciendis suscipit
+                    facere. Distinctio, aspernatur suscipit.
+                    "</p>
+                <div class="flex flex-col text-center mt-4">
+                    <span>Irfan M. Silawane</span>
+                    <span>Kepala Sekolah</span>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="flex max-w-screen-lg mx-auto">
+
+        </div>
+    </div>
 @endsection
 
 @section('script')
