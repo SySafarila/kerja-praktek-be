e530d7f84475706f971ab2c09403be90884fae39
Muhammad Dagistan Silawane (Razes) <71701718+iamrazes@users.noreply.github.com>
2023-12-29 17:27:07 +0700

    added privacy policy

---
+++ resources/views/public/privacy-policy.blade.php
@@ -0,0 +1,169 @@
+@extends('layouts.public')
+
+@section('head')
+@endsection
+
+@section('content')
+    <div class="flex flex-col gap-y-8 my-12 ">
+        <div class="flex flex-col gap-y-4 max-w-screen-lg mx-auto mt-8">
+            <img src="{{ asset('images/logos/logo1a.png') }}" class="h-60 w-60 mx-auto" alt="">
+            <h1 class="text-3xl font-bold text-center">SMA MA'ARIF PACET<br> CIANJUR</h1>
+        </div>
+        <div class="flex max-w-screen-lg mx-auto">
+            <div class="flex flex-col gap-y-4 px-4">
+                <div class="mb-4">
+                    <p class="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut
+                        possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate
+                        odioLorem, ipsum dolor sit amet consectetur adipisicing elit. Quos quisquam ut possimus nulla,
+                        cupiditate</p>
+                </div>
+                <div>
+                    <h2 class="font-bold text-accent-1 text-xl">PRIVACY & POLICY</h2>
+                    <div class="border-b-4 border-accent-1 mb-2"></div>
+                    <div class="text-justify">
+                        <b>
+                            Kebijakan Privasi untuk SMA Ma'arif Pacet Cianjur
+                        </b>
+                        <br />
+                        <br />
+                        Terakhir diperbarui: 29/12/2023
+                        <br />
+                        <br />
+                        SMA Ma'arif Pacet Cianjur mengoperasikan website <a href="https://smamaarifpacet.sch.id" class="text-accent-1">smamaarifpacet.sch.id</a>. Halaman ini memberi tahu Anda mengenai kebijakan kami terkait
+                        pengumpulan, penggunaan, dan pengungkapan Informasi Pribadi ketika Anda menggunakan layanan kami.
+                        <br />
+                        Kami menggunakan Informasi Pribadi Anda untuk menyediakan dan meningkatkan Layanan. Dengan
+                        menggunakan Layanan, Anda menyetujui pengumpulan dan penggunaan informasi sesuai dengan kebijakan
+                        ini.
+                        <br />
+                        <br />
+                        <h1 class="text-xl font-bold">
+                            1. Pengumpulan dan Penggunaan Informasi
+                        </h1>
+                        Kami mengumpulkan beberapa jenis informasi yang berbeda untuk berbagai tujuan guna menyediakan dan
+                        meningkatkan Layanan kami kepada Anda. Jenis Data yang Dikumpulkan meliputi:
+                        <br />
+                        <br />
+                        <b>
+                            Data Pribadi:</b>
+                        <br />
+                        Saat menggunakan Layanan kami, kami mungkin meminta Anda untuk memberikan beberapa informasi yang
+                        dapat diidentifikasi secara pribadi yang dapat digunakan untuk menghubungi atau mengidentifikasi
+                        Anda ("Data Pribadi"). Informasi yang dapat diidentifikasi secara pribadi dapat mencakup, namun
+                        tidak terbatas pada:
+                        <ul style="list-style-type: circle;" class="pl-6 mt-1">
+                            <li>
+                                Alamat email
+                            </li>
+                            <li>
+                                Nama depan dan belakang
+                            </li>
+                            <li>
+                                Nomor telepon
+                            </li>
+                            <li>
+                                Alamat, Provinsi, Kode Pos, Kota
+                            </li>
+                            <li>
+                                Cookie dan Data Pengguna
+                            </li>
+                            <li>
+                                Cookie dan Data Pengguna
+                            </li>
+                        </ul>
+                        <br />
+                        <b>
+                            Data Penggunaan:
+                        </b>
+
+                        <br />
+                        Kami mungkin mengumpulkan informasi tentang cara Anda mengakses dan menggunakan Layanan ("Data
+                        Penggunaan"). Data Penggunaan ini dapat mencakup informasi seperti alamat Protokol Internet Anda
+                        (IP), jenis perangkat, jenis browser, halaman Layanan kami yang Anda kunjungi, waktu dan tanggal
+                        kunjungan Anda, waktu yang dihabiskan pada halaman tersebut, dan informasi lainnya.
+                        <br />
+                        <br />
+                        <h1 class="text-xl font-bold">
+                            2. Penggunaan Informasi
+                        </h1>
+                        <section>
+                            Kami menggunakan informasi yang kami kumpulkan untuk berbagai tujuan, termasuk:
+                            <br />
+                            <ul style="list-style-type: lower-alpha;" class="pl-6 mt-1">
+                                <li>
+                                    Menyediakan dan Memelihara Layanan
+                                    <br/>
+                                    Menggunakan informasi untuk memberikan Layanan kepada Anda dan memelihara serta meningkatkan kualitas Layanan kami.
+                                </li>
+                                <li>
+                                    Pemberitahuan Perubahan
+                                    <br />
+                                    Memberi tahu Anda tentang perubahan yang dilakukan pada Layanan kami.
+                                </li>
+                                <li>
+                                    Komunikasi
+                                    <br />
+                                    Menghubungi Anda melalui email, surat, atau pemberitahuan serupa terkait Layanan, pemberitahuan,
+                                    dan
+                                    penawaran promosi, kecuali Anda memilih untuk tidak menerima komunikasi tersebut.
+                                </li>
+                            </ul>
+                            <br />
+                            <h1 class="text-xl font-bold">
+                            3. Pengungkapan Informasi
+                            </h1>
+                            Kami dapat mengungkapkan Informasi Pribadi Anda dalam situasi tertentu, termasuk:
+                            <br />
+                            a. Kewajiban Hukum
+                            Untuk mematuhi kewajiban hukum.
+                            <br />
+                            b. Perlindungan Keamanan
+                            Melindungi keamanan dan integritas Layanan kami.
+                            <br />
+                            <br />
+                            <h1 class="text-xl font-bold">
+                            4. Keamanan
+                            </h1>
+                            Kami akan menjaga keamanan Informasi Pribadi Anda dan mengambil langkah-langkah yang wajar untuk
+                            melindunginya dari akses yang tidak sah atau pengungkapan.
+                            <br />
+                            <br />
+                            <h1 class="text-xl font-bold">
+                            5. Perubahan pada Kebijakan Privasi Ini
+                            </h1>
+                            Kami dapat memperbarui Kebijakan Privasi kami dari waktu ke waktu. Kami akan memberi tahu Anda
+                            tentang perubahan dengan mempublikasikan Kebijakan Privasi yang baru di halaman ini.
+                            <br />
+                            <h1 class="text-xl font-bold">
+                            <br />
+                            6. Hubungi Kami
+                            </h1>
+                            Jika Anda memiliki pertanyaan atau saran tentang Kebijakan Privasi kami, hubungi kami di:
+                            <br />
+                            <br />
+                            Alamat : Jl.Raya Pacet No.30 RT/RW 04/04 Kp. Pasircina Desa Cipendawa Kecamatan Pacet Kabupate Cianjur Jawa Barat 43253
+                            <br />
+                            <br />
+                            Alamat Email : sma.maarif.pacet@gmail.com
+                            <br />
+                            <br />
+                            Nomor Telepon : +62 856-2485-7093
+                            <br />
+                            <br />
+                            Terima kasih telah mempercayai kami dengan Informasi Pribadi Anda. Kami berkomitmen untuk
+                            menjaga
+                            privasi dan keamanan informasi Anda.
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+@section('script')
+@endsection
